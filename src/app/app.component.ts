0f726778d7d3cca3c102e36fe01568455e7dc3c5
AkashAaki <91463410+AkashAaki@users.noreply.github.com>
2024-01-10 18:33:54 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterLink,
+    RouterOutlet, 
+    HomeComponent, 
+    RouterModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
